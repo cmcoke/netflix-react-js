aa1ded460c952668e6e435bd348a7f4419eaac67
cmcoke <minoru.cm@gmail.com>
2023-12-21 16:10:37 -0500

    configured authentication using firebase.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 };
